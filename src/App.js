9606a5da0519fa33c9b53f1676e33147aa4ec25e
fperez99 <fperez@iesrafaelalberti.es>
2022-01-14 12:15:27 +0100

    Bienvenidos

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>
-          Hola Mundo
+          Hola Mundo, Bienvenidos
         </h1>
         <a
           className="App-link"
