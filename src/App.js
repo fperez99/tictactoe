b02273896446484e4946e3e7366c36d436fee265
fperez99 <fperez@iesrafaelalberti.es>
2022-01-14 12:20:03 +0100

    Se agrega un nuevo mensaje

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>
-          Hola Mundo, Bienvenidos
+          Hola Mundo, Bienvenidos a todos
         </h1>
         <a
           className="App-link"
